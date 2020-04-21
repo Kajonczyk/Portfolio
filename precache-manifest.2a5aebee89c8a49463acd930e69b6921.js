6413696eb682db40a62a37bb93e3190f6d3a6d40
Kajonczyk <kopczackikamil2000@gmail.com>
2020-04-21 18:40:34 +0200

    Updates

---
+++ precache-manifest.2a5aebee89c8a49463acd930e69b6921.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba88f81aa98e110df2f65e78598a6999",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a1f84a6d46d49dd75e7e",
+    "url": "/portfolio/static/js/2.ac00876c.chunk.js"
+  },
+  {
+    "revision": "723894b771c1fe011b0df55ec646ea2d",
+    "url": "/portfolio/static/js/2.ac00876c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9ccc4bd74d451cb8b71",
+    "url": "/portfolio/static/js/main.51e3e742.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "1eea720e4da6a2e489b87c8ca29e73ca",
+    "url": "/portfolio/static/media/Shape.1eea720e.svg"
+  },
+  {
+    "revision": "c1e87c1bc0914cdfaa26dd57773b7d87",
+    "url": "/portfolio/static/media/hawksmandesktop.c1e87c1b.png"
+  },
+  {
+    "revision": "4757960a931d520793c628eae6064eda",
+    "url": "/portfolio/static/media/hawksmanmobile.4757960a.png"
+  },
+  {
+    "revision": "29d35245fcd6f2836f924bfdef5731bc",
+    "url": "/portfolio/static/media/hawksmantablet.29d35245.png"
+  },
+  {
+    "revision": "121dc3917498475cb4f10058cd59827f",
+    "url": "/portfolio/static/media/juniorstartdesktop.121dc391.png"
+  },
+  {
+    "revision": "07675bf857de9060f6bbf1f41512c5a7",
+    "url": "/portfolio/static/media/juniorstartmobile.07675bf8.png"
+  },
+  {
+    "revision": "2016f3a8e246a791656e52ff8c880532",
+    "url": "/portfolio/static/media/juniorstarttablet.2016f3a8.png"
+  },
+  {
+    "revision": "f4349d520ba57d62e0c830390986b506",
+    "url": "/portfolio/static/media/youviddesktop.f4349d52.png"
+  },
+  {
+    "revision": "09f000784f2890782b9409caeaf6b5d1",
+    "url": "/portfolio/static/media/youvidmobile.09f00078.png"
+  },
+  {
+    "revision": "853b35ab09257c164ed0b777ff99dfb2",
+    "url": "/portfolio/static/media/youvidtablet.853b35ab.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.e9f57ffcb66758256e012c0acf057f43.js"
+  "/portfolio/precache-manifest.2a5aebee89c8a49463acd930e69b6921.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
