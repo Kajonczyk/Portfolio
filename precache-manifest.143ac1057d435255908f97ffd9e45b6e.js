d094ef6651472a1f17afeb7438b6c6b110e4eb53
Kajonczyk <kopczackikamil2000@gmail.com>
2020-04-21 18:35:41 +0200

    Updates

---
+++ precache-manifest.143ac1057d435255908f97ffd9e45b6e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28aebb0d35faff89642fb0db63c7e674",
+    "url": "/nazwa/index.html"
+  },
+  {
+    "revision": "397d64a0f4a4cce68c92",
+    "url": "/nazwa/static/js/2.2ac8520b.chunk.js"
+  },
+  {
+    "revision": "723894b771c1fe011b0df55ec646ea2d",
+    "url": "/nazwa/static/js/2.2ac8520b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9ccc4bd74d451cb8b71",
+    "url": "/nazwa/static/js/main.51e3e742.chunk.js"
+  },
+  {
+    "revision": "d8f920be4789930729c8",
+    "url": "/nazwa/static/js/runtime-main.996f9d6e.js"
+  },
+  {
+    "revision": "1eea720e4da6a2e489b87c8ca29e73ca",
+    "url": "/nazwa/static/media/Shape.1eea720e.svg"
+  },
+  {
+    "revision": "c1e87c1bc0914cdfaa26dd57773b7d87",
+    "url": "/nazwa/static/media/hawksmandesktop.c1e87c1b.png"
+  },
+  {
+    "revision": "4757960a931d520793c628eae6064eda",
+    "url": "/nazwa/static/media/hawksmanmobile.4757960a.png"
+  },
+  {
+    "revision": "29d35245fcd6f2836f924bfdef5731bc",
+    "url": "/nazwa/static/media/hawksmantablet.29d35245.png"
+  },
+  {
+    "revision": "121dc3917498475cb4f10058cd59827f",
+    "url": "/nazwa/static/media/juniorstartdesktop.121dc391.png"
+  },
+  {
+    "revision": "07675bf857de9060f6bbf1f41512c5a7",
+    "url": "/nazwa/static/media/juniorstartmobile.07675bf8.png"
+  },
+  {
+    "revision": "2016f3a8e246a791656e52ff8c880532",
+    "url": "/nazwa/static/media/juniorstarttablet.2016f3a8.png"
+  },
+  {
+    "revision": "f4349d520ba57d62e0c830390986b506",
+    "url": "/nazwa/static/media/youviddesktop.f4349d52.png"
+  },
+  {
+    "revision": "09f000784f2890782b9409caeaf6b5d1",
+    "url": "/nazwa/static/media/youvidmobile.09f00078.png"
+  },
+  {
+    "revision": "853b35ab09257c164ed0b777ff99dfb2",
+    "url": "/nazwa/static/media/youvidtablet.853b35ab.png"
+  }
+]);
